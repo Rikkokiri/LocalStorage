b0f44d0013ebf313c12c97ec85f281623b435242
Simranjit Singh <singhz.galaxy@gmail.com>
2020-12-07 19:41:40 +0200

    Detect browser & OS
    Detecting user's browser and operating system details. and store as JSON objects.

--- script.js
+++ script.js
@@ -40,3 +40,48 @@ function greetUser () {
         $("#hidable-form summary").text("Wrong name? Reintroduce yourself!")
     }
 }
+
+window.addEventListener("load", function () {
+    //Parse User Agent
+    var userBrowser = bowser.getParser(navigator.userAgent).getResult();
+
+    //Browser information for logging
+    /*console.log(userBrowser.browser.name);
+    console.log(userBrowser.browser.version);
+    console.log(userBrowser.engine);*/
+    
+    //Save User's browser information in an object
+    var browserInfo = {
+        Name: userBrowser.browser.name,
+        Version: userBrowser.browser.version,
+        Engine: userBrowser.engine
+    };
+
+    //Convert object to string for storage
+    var browserInfoStr = JSON.stringify(browserInfo);
+
+    //Store User's browser information in localStorage
+    localStorage.setItem("Browser", browserInfoStr);
+
+    //OS information for logging
+    /*console.log(userBrowser.os.name);
+    console.log(userBrowser.os.version);
+    console.log(userBrowser.os.versionName);*/
+
+    //Save User's OS information in an object
+    var osInfo = {
+        Name: userBrowser.os.name,
+        Version: userBrowser.os.version,
+        Engine: userBrowser.os.versionName
+    };
+
+    //Convert object to string for storage
+    var osInfoStr = JSON.stringify(osInfo);
+
+    //Store User's OS information in localStorage
+    localStorage.setItem("OS", osInfoStr);
+
+    //Platform
+    console.log(userBrowser.platform.type);
+    localStorage.setItem("Platform", userBrowser.platform.type);
+});
