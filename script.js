a3ce5210bc7490a61e85f6c3e98bb41482946b2e
Ossian Rajala <ossian.rajala@gmail.com>
2020-12-08 15:48:25 +0200

    Fix form submission

--- script.js
+++ script.js
@@ -81,6 +81,7 @@ function addListeners() {
 
   // Handle firstname in admin form submission
   $('#userInfFirstName').submit(function (event) {
+    event.preventDefault();
     // Store user's name in local storage
     let fNameVal = $('#fName').val();
     if (fNameVal !== '') {
